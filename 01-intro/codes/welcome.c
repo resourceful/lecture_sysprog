28d570105340c9e75552668bff73e34a4c23d307
Seongjin Lee <jamesbrother@gmail.com>
2016-09-06 18:40:51 +0900

    week 1 day 1 system programming, history, basics, editors (vi, emacs)

---
+++ 01-intro/codes/welcome.c
@@ -0,0 +1,10 @@
+/* This is a broken example, and you will have to fix the errors in the file to run the program. 
+ */
+
+#include <stdio.h>
+
+int
+main(int argc, char **argv) {
+	printf("Welcome to System Programming, %s!\n", getlogin())
+	return 0;
+}
