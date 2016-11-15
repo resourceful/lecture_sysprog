3cdcfe6c8f423fa0dd85b7bb5d7b77b05338115f
Seongjin Lee <jamesbrother@gmail.com>
2016-11-15 23:05:32 +0900

    08_thread

---
+++ 08_thread/codes/print_thrID.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+pthread_t ntid;
+
+void
+printids(const char *s)
+{
+    pid_t       pid;
+    pthread_t   tid;
+    pid = getpid();
+    tid = pthread_self();
+    printf("%s pid %lu tid %lu (0x%lx)\n", s, (unsigned long)pid,
+      (unsigned long)tid, (unsigned long)tid);
+}
+
+void *
+thr_fn(void *arg)
+{
+    printids("new thread: ");
+    return((void *)0);
+}
+
+int
+main(void)
+{
+int err;
+    err = pthread_create(&ntid, NULL, thr_fn, NULL);
+    if (err != 0){
+        fprintf(stderr, "can’t create thread");
+	}
+    printids("main thread:");
+    sleep(1);
+    exit(0);
+}
